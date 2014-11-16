46c8c318d7346e431f68b2bb47c336fb80a8cd35
Junichi Ito <jit@sonicgarden.jp>
2014-11-17 05:28:05 +0900

    Add validation.

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,5 @@
 class OrderDetail < ActiveRecord::Base
   belongs_to :order
+  validates :item_name, :unit_price, :quantity, presence: true
+  validates :unit_price, :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
