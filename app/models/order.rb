4aa2c4d5a547c6cb33f0d6c04e0d57d1b143a5fa
Junichi Ito <jit@sonicgarden.jp>
2014-11-17 07:06:35 +0900

    Add comment.

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,8 +9,8 @@ class Order < ActiveRecord::Base
   # http://ayaketan.hatenablog.com/entry/2014/01/26/180141
   def save_details_from_csv(csv_file)
     csv_text = csv_file.read
-
     CSV.parse(Kconv.toutf8(csv_text)) do |row|
+      # 本来であればカラム数のチェック等を実施すべきだが、サンプルコードなのでここでは割愛する
       self.order_details.create(item_name: row[0], unit_price: row[1], quantity: row[2])
     end
   end
